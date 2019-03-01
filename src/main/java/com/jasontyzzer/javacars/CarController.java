10137b48e9608c88c45342c0c9ecff2cabef0c17
JTyzz <jtyzzer@gmail.com>
2019-03-01 09:45:55 -0800

    Added /year and /brand gets and tested CarController class and everything seems to work

---
+++ src/main/java/com/jasontyzzer/javacars/CarController.java
@@ -0,0 +1,73 @@
+package com.jasontyzzer.javacars;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/cars/")
+public class CarController {
+    private final CarRepository carRepo;
+    private final RabbitTemplate rabbitTemplate;
+
+    public CarController(CarRepository carRepo, RabbitTemplate rabbitTemplate) {
+        this.carRepo = carRepo;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    // id/{id}
+    @GetMapping  ("id/{id}")
+    public Car findCar(@PathVariable Long id){
+        Car car = carRepo.findById(id).orElseThrow();
+        return car;
+    }
+
+    // year/{year}
+    @GetMapping ("year/{year}")
+    public List<Car> getCarByYear(@PathVariable String year){
+        List<Car> carList = carRepo.findAll();
+        List<Car> carYear = new ArrayList<>();
+
+        int yearInt = Integer.parseInt(year);
+        for (Car c : carList){
+            if(c.getYear() == yearInt){
+                carYear.add(c);
+            }
+        }
+        return carYear;
+    }
+
+    // brand/{brand}
+    @GetMapping ("brand/{brand}")
+    public List<Car> getCarByBrand(@PathVariable String brand){
+        List<Car> carList = carRepo.findAll();
+        List<Car> carBrand= new ArrayList<>();
+
+        for (Car c : carList){
+            if(c.getBrand().toLowerCase().equals(brand.toLowerCase())){
+                carBrand.add(c);
+            }
+        }
+        return carBrand;
+    }
+    // upload
+    @PostMapping ("upload")
+    public List<Car> loadCarData(@RequestBody List<Car> newData){
+        return carRepo.saveAll(newData);
+    }
+
+    // delete/{id}
+    @DeleteMapping ("delete/{id}")
+    public Car deleteCar(@PathVariable Long id){
+        Car car = carRepo.findById(id).orElseThrow();
+        carRepo.delete(car);
+        CarLog message = new CarLog(id + "Data Deleted");
+        rabbitTemplate.convertAndSend(JavaCarsApplication.QUEUE_NAME, message.toString());
+        log.info( id+ "Data Deleted");
+        return car;
+    }
+}
